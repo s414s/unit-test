5a8416da6d69798860f8b368d39ae9319cede949
Alberto Salas <alsalasher@gmail.com>
2023-04-14 22:37:20 +0200

    create main function

--- unitesting/src/main/java/org/sanvalero/ConversorNumerosRomanos.java
+++ unitesting/src/main/java/org/sanvalero/ConversorNumerosRomanos.java
@@ -2,6 +2,25 @@ package org.sanvalero;
 
 public class ConversorNumerosRomanos {
 
+    public String convertirARomano(int numeroDecimal) {
+        if (numeroDecimal < 1 || numeroDecimal > 3000) {
+            throw new IllegalArgumentException("El número decimal debe estar entre 1 y 3000");
+        }
 
+        int[] VALORES_DECIMALES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] NUMEROS_ROMANOS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder romanNumber = new StringBuilder();
+        int remainingValue = numeroDecimal;
+
+        for (int i = 0; i < VALORES_DECIMALES.length; i++) {
+            while (remainingValue >= VALORES_DECIMALES[i]) {
+                romanNumber.append(NUMEROS_ROMANOS[i]);
+                remainingValue -= VALORES_DECIMALES[i];
+            }
+        }
+
+        return romanNumber.toString();
+    }
 
 }
--- unitesting/src/main/java/org/sanvalero/Main.java
+++ unitesting/src/main/java/org/sanvalero/Main.java
@@ -2,6 +2,14 @@ package org.sanvalero;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        ConversorNumerosRomanos conversor = new ConversorNumerosRomanos();
+        System.out.println(conversor.convertirARomano(1));
+        System.out.println(conversor.convertirARomano(10));
+        System.out.println(conversor.convertirARomano(7));
+        System.out.println(conversor.convertirARomano(1949));
+        System.out.println(conversor.convertirARomano(2018));
     }
 }
