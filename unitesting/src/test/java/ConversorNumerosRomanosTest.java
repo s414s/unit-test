a7bb6f1ceb21742a7bd6b3f2878a533b790d9418
Alberto Salas <alsalasher@gmail.com>
2023-04-14 22:39:34 +0200

    update create an instance of class to call method

--- unitesting/src/test/java/ConversorNumerosRomanosTest.java
+++ unitesting/src/test/java/ConversorNumerosRomanosTest.java
@@ -9,18 +9,18 @@ public class ConversorNumerosRomanosTest {
 
     @Test
     public void testConvertToRoman() {
-        assertEquals("I", ConversorNumerosRomanos.convertirARomano(1));
-        assertEquals("X", ConversorNumerosRomanos.convertirARomano(10));
-        assertEquals("VII", ConversorNumerosRomanos.convertirARomano(7));
-        assertEquals("MCMXLIX", ConversorNumerosRomanos.convertirARomano(1949));
-        assertEquals("MMXVIII", ConversorNumerosRomanos.convertirARomano(2018));
+        assertEquals("I", new ConversorNumerosRomanos().convertirARomano(1));
+        assertEquals("X", new ConversorNumerosRomanos().convertirARomano(10));
+        assertEquals("VII", new ConversorNumerosRomanos().convertirARomano(7));
+        assertEquals("MCMXLIX", new ConversorNumerosRomanos().convertirARomano(1949));
+        assertEquals("MMXVIII", new ConversorNumerosRomanos().convertirARomano(2018));
     }
 
     @Test
     public void testConvertToRomanWithInvalidInput() {
-        assertThrows(IllegalArgumentException.class, () -> ConversorNumerosRomanos.convertirARomano(0));
-        assertThrows(IllegalArgumentException.class, () -> ConversorNumerosRomanos.convertirARomano(-1));
-        assertThrows(IllegalArgumentException.class, () -> ConversorNumerosRomanos.convertirARomano(3001));
+        assertThrows(IllegalArgumentException.class, () -> new ConversorNumerosRomanos().convertirARomano(0));
+        assertThrows(IllegalArgumentException.class, () -> new ConversorNumerosRomanos().convertirARomano(-1));
+        assertThrows(IllegalArgumentException.class, () -> new ConversorNumerosRomanos().convertirARomano(3001));
     }
 
 }
