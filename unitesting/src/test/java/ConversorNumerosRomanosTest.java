17a3d05be878cb516547571fe2b8c667252be50a
Alberto Salas <alsalasher@gmail.com>
2023-04-14 22:29:14 +0200

    create test

--- unitesting/src/test/java/ConversorNumerosRomanosTest.java
+++ unitesting/src/test/java/ConversorNumerosRomanosTest.java
@@ -7,6 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ConversorNumerosRomanosTest {
 
+    @Test
+    public void testConvertToRoman() {
+        assertEquals("I", ConversorNumerosRomanos.convertirARomano(1));
+        assertEquals("X", ConversorNumerosRomanos.convertirARomano(10));
+        assertEquals("VII", ConversorNumerosRomanos.convertirARomano(7));
+        assertEquals("MCMXLIX", ConversorNumerosRomanos.convertirARomano(1949));
+        assertEquals("MMXVIII", ConversorNumerosRomanos.convertirARomano(2018));
+    }
 
+    @Test
+    public void testConvertToRomanWithInvalidInput() {
+        assertThrows(IllegalArgumentException.class, () -> ConversorNumerosRomanos.convertirARomano(0));
+        assertThrows(IllegalArgumentException.class, () -> ConversorNumerosRomanos.convertirARomano(-1));
+        assertThrows(IllegalArgumentException.class, () -> ConversorNumerosRomanos.convertirARomano(3001));
+    }
 
 }
